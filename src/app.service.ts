ace1042c4666d7b321a20a878cbb06041b780d61
Cefalo Giovanni <giovanni.cefalo@gmail.com>
2025-05-16 00:22:58 +0200

    format - lint

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService 
 {
-  getHello(): string 
-{
-    return 'Hello World!';
-  }
+    getHello(): string 
+    {
+        return 'Hello World!';
+    }
 }
