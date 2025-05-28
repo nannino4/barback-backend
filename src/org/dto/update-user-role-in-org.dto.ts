56ac057a3924a5a8226db5590db68c77bed309c2
Cefalo Giovanni <giovanni.cefalo@gmail.com>
2025-05-28 11:19:55 +0200

    - org - sub

--- src/org/dto/update-user-role-in-org.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsEnum } from 'class-validator';
-import { OrgRole } from '../schemas/user-org.schema';
-
-export class UpdateUserRoleInOrgDto
-{
-    @IsEnum(OrgRole)
-    role!: OrgRole;
-}
